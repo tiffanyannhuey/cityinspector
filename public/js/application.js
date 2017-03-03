4f422b7018a1cc57868a62fa4c35b43bd93e0629
tiffanyannhuey <87tiffanyhuey@gmail.com>
2017-03-03 12:17:40 -0800

    add columns

--- public/js/application.js
+++ public/js/application.js
@@ -21,12 +21,12 @@ $(document).ready(function() {
       $('li').remove();
       var listItem = "<li></li>"
       console.log(response);
-      console.log(response["data"]["widget"]);
+      console.log(response["widget"]);
       // console.log(response["data"]["categories"][0]["name"])
       var data = response["data"]["categories"];
       for(i=0; i< data.length-2; i++ ){
         $('ul').append("<li style="+data[i]["color"]+">"+data[i]["name"]+ ": "+ data[i]["score_out_of_10"]+"</li>")}
-
+      $(".info").append(response["widget"])
       // console.log($('form#find-city'))
       $('form#find-city')[0].reset();
     })
